f66e0c0a25c7b71171bc6a1154f15f7728ee1369
Nicolas Buffon <akara@pixeized.fr>
2013-07-14 18:17:06 +0200

    IA changed to AI

---
+++ AI.cpp
@@ -0,0 +1,19 @@
+#include "AI.hpp"
+
+namespace QAwale {
+    namespace Core {
+        int evaluate(const Gameboard &state, int playerTaking, int opponentTaking, int playerNumber) {
+            int startHoleNumber = playerNumber == 2 ? 0 : 6;
+            int endHoleNumber = playerNumber == 2 ? 5 : 11;
+            int seedCount = 0;
+
+            for (int i = startHoleNumber; i <= endHoleNumber; ++i)
+                seedCount += state.getHoleSeedCount(i);
+
+            if (seedCount > 0)
+                return playerTaking - opponentTaking;
+            else
+                return -1000;
+        }
+    }
+}
---
+++ AI.hpp
@@ -0,0 +1,18 @@
+#ifndef __AI_HPP__
+#define __AI_HPP__
+
+#include "Gameboard.hpp"
+
+namespace QAwale {
+    namespace Core {
+        enum PlayerType {
+            Person,
+            EasyAI,
+            HardAI
+        };
+
+        int evaluate(const Gameboard& state, int playerTaking, int opponentTaking, int number);
+    }
+}
+
+#endif // __AI_HPP__
